1019d26a0eaca3d8f25d3ac02cb50e918b2f6942
nateeatsrice <36753674+nateeatsrice@users.noreply.github.com>
2025-09-11 20:15:15 +0000

    updated python script for orchestration

--- 03-deployment/duration-prediction.py
+++ 03-deployment/duration-prediction.py
@@ -11,19 +11,16 @@ from sklearn.feature_extraction import DictVectorizer
 from sklearn.metrics import root_mean_squared_error
 
 import mlflow
-
-mlflow.set_tracking_uri("http://localhost:5000")
-mlflow.set_experiment("nyc-taxi-experiment")
-
-models_folder = Path('models')
-models_folder.mkdir(exist_ok=True)
+from prefect import flow, task
 
 
-#ingestion and feature engineering
-def read_dataframe(year, month):
+
+@task
+def read_data(year:int, month:int) -> pd.DataFrame:
+    '''ingestion and feature engineering'''
     url = f'https://d37ci6vzurychx.cloudfront.net/trip-data/green_tripdata_{year}-{month:02d}.parquet'
     df = pd.read_parquet(url)
-
+    print(f"[1] data loaded from {url}")
     df['duration'] = df.lpep_dropoff_datetime - df.lpep_pickup_datetime
     df.duration = df.duration.apply(lambda td: td.total_seconds() / 60)
 
@@ -33,25 +30,34 @@ def read_dataframe(year, month):
     df[categorical] = df[categorical].astype(str)
 
     df['PU_DO'] = df['PULocationID'] + '_' + df['DOLocationID']
-
+    print(f"[2] Number of records loaded: {len(df)}")
     return df
 
-#encode data and dict vectorizer
-def create_X(df, dv=None):
+@task
+def create_X(df:pd.DataFrame, dv=None):
+    '''encode data and create dict vectorizer'''
     categorical = ['PU_DO']
     numerical = ['trip_distance']
     dicts = df[categorical + numerical].to_dict(orient='records')
 
     if dv is None:
         dv = DictVectorizer(sparse=True)
+        print("[3] DictVectorizer Created")
         X = dv.fit_transform(dicts)
     else:
         X = dv.transform(dicts)
-
+    print(f"[4] Records after filtering: {len(X)}")
     return X, dv
 
-#train xgboost model
+@task
 def train_model(X_train, y_train, X_val, y_val, dv):
+    '''training xgboost and logging model'''
+
+    mlflow.set_tracking_uri("http://localhost:5000")
+    mlflow.set_experiment("nyc-taxi-experiment3")
+    # creates a directory named models in the current working directory. 
+    Path('models').mkdir(exist_ok=True)
+
     with mlflow.start_run() as run:
         train = xgb.DMatrix(X_train, label=y_train)
         valid = xgb.DMatrix(X_val, label=y_val)
@@ -75,26 +81,30 @@ def train_model(X_train, y_train, X_val, y_val, dv):
             evals=[(valid, 'validation')],
             early_stopping_rounds=50
         )
-
+        
         y_pred = booster.predict(valid)
         rmse = root_mean_squared_error(y_val, y_pred)
+        print(f"[5] Model trained. RMSE = {rmse}")
         mlflow.log_metric("rmse", rmse)
 
         with open("models/preprocessor.b", "wb") as f_out:
             pickle.dump(dv, f_out)
         mlflow.log_artifact("models/preprocessor.b", artifact_path="preprocessor")
 
-        mlflow.xgboost.log_model(booster, artifact_path="models_mlflow")
+        mlflow.xgboost.log_model(booster, artifact_path="model")
 
-        return run.info.run_id
+        run_id = run.info.run_id
+        print(f"[6] Model logged to MLflow. MLflow Run ID: {run_id}")
 
+        return run_id
 
-def run(year, month):
-    df_train = read_dataframe(year=year, month=month)
+@flow
+def run(year:int, month:int):
+    df_train = read_data(year=year, month=month)
 
     next_year = year if month < 12 else year + 1
     next_month = month + 1 if month < 12 else 1
-    df_val = read_dataframe(year=next_year, month=next_month)
+    df_val = read_data(year=next_year, month=next_month)
 
     X_train, dv = create_X(df_train)
     X_val, _ = create_X(df_val, dv)
@@ -104,7 +114,7 @@ def run(year, month):
     y_val = df_val[target].values
 
     run_id = train_model(X_train, y_train, X_val, y_val, dv)
-    print(f"MLflow run_id: {run_id}")
+    
     return run_id
 
 
